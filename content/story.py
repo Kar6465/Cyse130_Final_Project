1044efc204365f8a4dbd31e04c872d5c5cb3ce34
Srikar Dontula <finstudent2020@gmail.com>
2026-05-01 10:02:43 -0400

    Update story.py

--- content/story.py
+++ content/story.py
@@ -10,7 +10,7 @@ def castle_halls():
     return {
         'text': 'You slip into the castle halls. Smoke fills the air. Knights and servants rush past. Three paths lie before you.',
         'choices': [
-            {'text': 'Find the heir and restore the kingdom', 'next': 'escape_path'},
+            {'text': 'Find the heir and restore the kingdom', 'next': 'great_hall'},
             {'text': 'Seek power through secrets', 'next': 'claim_power'},
             {'text': 'Help the survivors escape', 'next': 'uncover_truth'}
         ]
@@ -19,8 +19,14 @@ def castle_halls():
 def great_hall():
     return {'text': 'The Great Hall looms above you as you walk in. You see a man standing in the center of the hall, looking you up and down. This was, as you recognized, Ser Kael',
             'choices': [
-            {'text': '', 'next': 'confront_usurper'},
-            {'text': 'Rescue the heir', 'next': 'rescue_heir'},
-            {'text': 'Gather allies and plan a rebellion', 'next': 'plan_rebellion'}
+            {'text': 'Prove Loyalty To Ser Kael', 'next': 'royal_chapel'},
+            {'text': 'Refuse and Walk Away', 'next': 'castle_halls'},
         ]
     }
+
+def royal_chapel():
+    return {'text': 'Ser Keal nods approvingly and leads you to the Royal Chapel, where you find Lady Elira kneeling before an altar. She looks up at you with a mix of hope and desperation.',
+            'choices': [
+                {'text'}
+            ]
+            }
